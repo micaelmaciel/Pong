53cb65d6a1fb193e321f5c35019df3b8a240cd2a
Michael <ragnarokz440@gmail.com>
2023-11-27 19:16:50 -0300

    Ball movement completed

--- Scenes/ball.gd
+++ Scenes/ball.gd
@@ -1,27 +1,40 @@
 extends CharacterBody2D
 
-var speed: int = 100
+@export var initialSpeed: int = 100
+@export var speedGain: int = 10
 
-func _ready() -> void:
+
+func initialize_ball() -> void:
 	randomize()
+	position = Vector2(240, 135)
 	var initalDirection: int = randi_range(0, 1)
 
 	if initalDirection:
-		velocity = Vector2(1, 0) * speed
+		velocity = Vector2(1, 0) * initialSpeed
 	else:
-		velocity = Vector2(-1, 0) * speed
+		velocity = Vector2(-1, 0) * initialSpeed
+
+func _ready() -> void:
+	initialize_ball()
+
 
 func _physics_process(_delta) -> void:
 	move_and_slide()
+	print(velocity)
 
 func _on_area_2d_area_entered(area) -> void:
 	var body: Node = area.get_parent()
+	print(global_position.x)
 
-	if (body is CharacterBody2D):
+	if (body is CharacterBody2D) and (global_position.x + 6 <= 415 and global_position.x - 6 >= 73):
 		if (velocity.y == 0):
-			velocity.y = speed/2
-		var spriteMiddle: int = body.global_position.y + body.get_node("Sprite2D").get_rect().size.y/2
-		var difference: int = global_position.y - spriteMiddle
+			velocity.y = initialSpeed/2
+		velocity.x += speedGain * sign(velocity.x)
+		velocity.y += speedGain * sign(velocity.y)
 		velocity.x = velocity.x * -1
-		velocity.x += 20
-		velocity.y += 20
+	elif (body is StaticBody2D):
+		velocity.y *= -1
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	initialize_ball()
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,8 +1,18 @@
 extends CharacterBody2D
 
+@export var speed: int = 70
+
+func handle_movement():
+	var direction: int = 0
+	if (Input.is_action_pressed("p1Down")):
+		direction = 1
+	elif (Input.is_action_pressed("p1Up")):
+		direction = -1
+	velocity.y = direction * speed
 
 func _ready():
 	pass
 
 func _physics_process(_delta):
-	pass
+	handle_movement()
+	move_and_slide()
